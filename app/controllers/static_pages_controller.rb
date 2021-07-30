7837ecc1db6fb0112055ff1ac43d87b431ca402a
berubenic <berubenic@gmail.com>
2021-07-30 16:16:09 -0400

    API returns Object, use #as_json, loop through each photo and build URL

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
     require 'flickr'
-    def show
-        
+
+    def index
+        flickr = Flickr.new
+
+        if params[:profile_id].present?
+        response = flickr.people.getPublicPhotos :user_id => params[:profile_id]
+        @list = response.as_json
+        end
     end
 end
